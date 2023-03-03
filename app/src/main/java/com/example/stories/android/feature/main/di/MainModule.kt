c50e1c6421be7a56d3d1833189489c8b0a5296d8
Godoken77 <mikhailplekhanov7@gmail.com>
2023-03-03 06:10:43 +0300

    TI-7 add category state and recently stories state to Main screen

--- app/src/main/java/com/example/stories/android/feature/main/di/MainModule.kt
+++ app/src/main/java/com/example/stories/android/feature/main/di/MainModule.kt
@@ -1,5 +1,7 @@
 package com.example.stories.android.feature.main.di
 
+import com.example.stories.android.feature.main.domain.usecase.RecentlyStoriesUseCase
+import com.example.stories.android.feature.main.domain.usecase.RecentlyStoriesUseCaseImpl
 import com.example.stories.android.feature.main.domain.usecase.RecommendedStoriesUseCase
 import com.example.stories.android.feature.main.domain.usecase.RecommendedStoriesUseCaseImpl
 import dagger.Binds
@@ -13,4 +15,7 @@ internal interface MainModule {
 
     @Binds
     fun bindRecommendedStoriesUseCase(impl: RecommendedStoriesUseCaseImpl): RecommendedStoriesUseCase
+
+    @Binds
+    fun bindRecentlyStoriesUseCase(impl: RecentlyStoriesUseCaseImpl): RecentlyStoriesUseCase
 }
--- app/src/main/java/com/example/stories/android/feature/main/domain/MainSideEffect.kt
+++ app/src/main/java/com/example/stories/android/feature/main/domain/MainSideEffect.kt
@@ -1,8 +1,3 @@
 package com.example.stories.android.feature.main.domain
 
-import com.example.stories.android.feature.category.domain.model.Category
-
-sealed class MainSideEffect {
-    data class OpenStory(val storyId: String) : MainSideEffect()
-    data class OpenStoriesByCategory(val category: Category) : MainSideEffect()
-}
+sealed class MainSideEffect
